8345703bf7c6873f5d4885c9a8a6d46b1e45f9de
Core-commits <undecised@gmail.com>
2020-06-03 12:04:16 +0200

    lol

---
+++ start.js
@@ -0,0 +1,17 @@
+const nh = require('node-hill')
+
+nh.startServer({
+    gameId: 433, // Your game id here
+
+    port: 42480, // Your port id here (default is 42480)
+
+    local: false, // Whether or not your server is local
+
+    // map: './maps/mygame.brk', - Your .brk file location here
+    
+    scripts: './user_scripts', // Your .js files location
+
+    sandbox: {} // Your npm modules you want to add to the VM 
+
+    // For more help: https://meta_data.gitlab.io/node-hill/interfaces/gamesettings.html
+})
